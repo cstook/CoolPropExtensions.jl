4fb2814c8c2717782e91decd5906963c0ab5b7ef
Christopher Stook <chris@chrisstook.com>
2019-11-18 20:24:05 -0500

    isothermalwork broken

--- src/constant_property_path_integral.jl
+++ src/constant_property_path_integral.jl
@@ -1,11 +1,24 @@
 const density = Property("D")
 const pressuretemperature = InputPair("PT_INPUTS")
 
-function isothermalwork(fluid::WorkingFluid, temperature, pressure_initial, pressure_final; rtol=1e-8)
-    function ρ(pressure,fluid)
+function isothermalwork(fluid::WorkingFluid,
+                        temperature,
+                        pressure_initial,
+                        pressure_final;
+                        rtol=1e-8)
+    function abc(fluid,pressure)
         update(fluid, pressuretemperature, pressure, temperature)
         keyed_output(fluid, density)
     end
-    integeral, err = quadgk(x->1/ρ(x,fluid),pressure_initial,pressure_final,rtol)
+    integeral, err = quadgk(x->1/abc(fluid,x),pressure_initial,pressure_final,rtol)
     integeral
+end
+
+function adiabaticwork(fluid::WorkingFluid,
+                       pressure_initial,
+                       temperature_initial,
+                       temperature_final;
+                       rtol=1e-8)
+    update(fluid, pressuretemperature, pressure, temperature)
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,16 @@ using Test
     pressuretemperature = CoolPropExtensions.InputPair("PT_INPUTS")
     water = CoolPropExtensions.WorkingFluid("HEOS","Water")
     CoolPropExtensions.update(water, pressuretemperature, 101325, 300)
-    c_p = CoolPropExtensions.keyed_output(water, constant_pressure_specific_heat)
+    c_p = CoolPropExtensions.keyed_output(water,
+        constant_pressure_specific_heat)
     @test c_p≈4180.6357765560715
     mass_density = CoolPropExtensions.Property("Dmass")
     pressure = CoolPropExtensions.Property("P")
     temperature = CoolPropExtensions.Property("T")
-    pd = CoolPropExtensions.first_partial_deriv(water, mass_density, pressure, temperature)
+    pd = CoolPropExtensions.first_partial_deriv(water,
+        mass_density, pressure, temperature)
     @test pd≈4.489626212601196e-7
-    work = CoolPropExtensions.isothermalwork(WorkingFluid("HEOS","Water"),300.0, 10atm, 100atm, rtol=1e-8)
+    work = CoolPropExtensions.isothermalwork(WorkingFluid("HEOS","Water"),
+        300.0, 10atm, 100atm, rtol=1e-8)
     @test work ≈ 9128.426693091658
 end
